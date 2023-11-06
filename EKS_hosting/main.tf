a64e5ac87047baf2c49b958e93bc03d559139da1
ashu-5025 <ashutoshsahu.5025@gmail.com>
2023-11-06 15:35:39 +0530

    sg_modified

--- EKS_hosting/main.tf
+++ EKS_hosting/main.tf
@@ -40,7 +40,7 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.default_vpc_id
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   #   control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
 
